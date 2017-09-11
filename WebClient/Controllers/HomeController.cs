6e4d2411bcc5c78f1ba2f86dc9a24018285545de
Xiaojia He <bya@qlik.com>
2017-09-11 16:43:37 +0200

    preliminary commit

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri ("http://localhost:5000");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync("/servicecluster/getall").Result;
+                string stringData = response.Content.ReadAsStringAsync().Result;
+                var myDeserializedObjList = (List<ServiceCluster>)JsonConvert.DeserializeObject(stringData,typeof(List<ServiceCluster>));
+                return View(myDeserializedObjList);
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebClient/Models/ServiceCluster.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Qlik.Sense.ServiceClusterManager.Model.Services;
+
+namespace WebClient.Models
+{
+    public class SharedFolderSettings
+    {
+        public string RootFolder { get; set; }
+        public string AppFolder { get; set; }
+        public string StaticContentRootFolder { get; set; }
+        public string ArchivedLogsRootFolder { get; set; }
+
+        public void PreUpdate()
+        {
+            if (string.IsNullOrEmpty(RootFolder))
+            {
+                throw new ArgumentException("Invalid RootFolder");
+            }
+
+            if (string.IsNullOrEmpty(AppFolder))
+            {
+                AppFolder = Path.Combine(RootFolder, "Apps");
+            }
+
+            if (string.IsNullOrEmpty(StaticContentRootFolder))
+            {
+                StaticContentRootFolder = Path.Combine(RootFolder, "StaticContent");
+            }
+
+            if (string.IsNullOrEmpty(ArchivedLogsRootFolder))
+            {
+                ArchivedLogsRootFolder = Path.Combine(RootFolder, "ArchivedLogs");
+            }
+        }
+    }
+
+    public class DatabaseConnectionSetting
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public int SSLPort { get; set; }
+
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public class ServiceDatabaseConnectionSettings : Dictionary<ServiceTypeEnum, DatabaseConnectionSetting> { }
+
+    public class ServiceCluster : PersistentModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public SharedFolderSettings SharedFolderSettings { get; set; }
+        public ServiceDatabaseConnectionSettings DatabaseConnectionSettings { get; set; }
+
+        public ServiceCluster()
+        {
+            SharedFolderSettings = new SharedFolderSettings();
+            DatabaseConnectionSettings = new ServiceDatabaseConnectionSettings();
+        }
+
+        public override void PreUpdate()
+        {
+            SharedFolderSettings.PreUpdate();
+            base.PreUpdate();
+        }
+    }
+}
