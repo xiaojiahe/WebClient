f1b6c18fdba09d181c0241037223997bd594d831
Xiaojia He <bya@qlik.com>
2017-09-12 19:11:05 +0200

    deleting, updating works too

---
+++ NewFolder/QlikServiceClusterManager.Model/Services/Service.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Qlik.Sense.ServiceClusterManager.Model.Services
+{
+	public interface IService : IPersistentModel
+	{
+		ServiceCluster ServiceCluster { get; set; }
+		string HostName { get; set; }
+		ServerNode ServerNode { get; set; }
+		ServiceTypeEnum ServiceType { get; }
+	}
+
+	public enum ServiceTypeEnum
+	{
+		Repository,
+		Proxy,
+		Scheduler,
+		Engine,
+		AppMigration,
+		Printing,
+	}
+
+    public abstract class Service : PersistentModel, IService
+    {
+	    [Required]
+		public ServiceCluster ServiceCluster { get; set; }
+
+		[Required]
+		public string HostName { get; set; }
+
+		public ServerNode ServerNode { get; set; }
+
+	    [Required]
+		public abstract ServiceTypeEnum ServiceType { get; }
+
+		protected Service() { }
+
+	    protected Service(ServiceCluster cluster, string hostName)
+	    {
+		    ServiceCluster = cluster;
+		    HostName = hostName;
+	    }
+	}
+}
