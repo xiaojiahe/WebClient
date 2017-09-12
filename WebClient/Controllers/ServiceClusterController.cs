1f3ff0e5a9c81157734236ef5d92ba52e9792a66
Xiaojia He <bya@qlik.com>
2017-09-12 10:52:04 +0200

    adding more routing

--- WebClient/Controllers/ServiceClusterController.cs
+++ WebClient/Controllers/ServiceClusterController.cs
@@ -1,49 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Newtonsoft.Json;
 using Qlik.Sense.ServiceClusterManager.Model;
+using Newtonsoft.Json;
 
 namespace WebClient.Controllers
 {
-    public class HomeController : Controller
+    
+    public class ServiceClusterController : Controller
     {
-        public ActionResult Index()
+        public ActionResult getAll()
         {
             using (HttpClient client = new HttpClient())
-            {  
+            {    
                 client.BaseAddress = new Uri("http://localhost:5000");
                 MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
                 client.DefaultRequestHeaders.Accept.Add(contentType);
                 HttpResponseMessage response = client.GetAsync("/servicecluster/getall").Result;
                 string stringData = response.Content.ReadAsStringAsync().Result;
                 var myDeserializedObjList = (List<ServiceCluster>)JsonConvert.DeserializeObject(stringData, typeof(List<ServiceCluster>));
-                ViewData["Mes"] = myDeserializedObjList[0].SharedFolderSettings.AppFolder;
                 return View(myDeserializedObjList);
             }
         }
 
-        public IActionResult About()
+        public ActionResult get(string id)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000");
+                MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync("/servicecluster/get"+ $"/{id}").Result;
+                string stringData = response.Content.ReadAsStringAsync().Result;
+                var myDeserialized = (ServiceCluster)JsonConvert.DeserializeObject(stringData, typeof(ServiceCluster));
+                return View(myDeserialized);
+            }
         }
     }
 }
